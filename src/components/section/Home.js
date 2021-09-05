d7c79d551763a3bdec1acc3d13347d1271b0b27a
Nguyễn Nhĩ Thái <62824044+nhithai4620@users.noreply.github.com>
2021-09-05 19:43:25 +0700

    thai commit at 7:43amm 9/5/2021

--- src/components/section/Home.js
+++ src/components/section/Home.js
@@ -3,6 +3,10 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";  
 import '../css/Home.css';
 import OrangeSlice from "../assets/orange-slice.png";
+import AppleControl from "../assets/apple_control.png";
+import OrangeControl from "../assets/orange_control.png";
+import BananaControl from "../assets/banana_control.png";
+import StrawControl from "../assets/straw_control.png";
 import {Link} from "react-router-dom";
 import {DataContext} from '../Context';
 
@@ -14,23 +18,38 @@ export class Home extends React.Component{
             return (
               <div id="home">
                 <div className="hero">
+
                   <div class="hero-overlay"></div>
+
                   <div class="bg"></div>
+
                   <div class="content">
                     <span className="sell-logan">Eat Fresh<br /> Stay Healthy...</span>
                     <p className="sell-content">Orange is not just a tasty fruit, its more. Get to know the multiple health benefits of adding oranges in your daily diet</p>
-                    <Link className="btn btn2">Buy Now</Link>
+                    <button className="btn btn2">Buy Now</button>
                   </div>
+
                   <div className="right">
                     <img src={OrangeSlice}></img>
                   </div>
-                  <div class="icon">
-                    <a href="#"><i class="fas fa-share-alt"></i></a>
-                    <a href="#"><i class="fab fa-facebook"></i></a>
-                    <a href="#"><i class="fab fa-pinterest"></i></a>
-                    <a href="#"><i class="fab fa-twitter"></i></a>
-                </div>
-                </div>
+
+                  <div class="fruit-control">
+                    <div className="fruit-control-item">
+                      <img id="apple" src={AppleControl} alt />
+                    </div>
+                    <div className="fruit-control-item">
+                      <img id="orange" src={OrangeControl} alt />
+                    </div>
+                    <div className="fruit-control-item">
+                      <img id="banana" src={BananaControl} alt />
+                    </div>
+                    <div className="fruit-control-item">
+                      <img id="straw" src={StrawControl} alt />
+                    </div>
+                  </div>
+
+                </div> 
+                {/* end class hero */}
               </div>
             );  
           } 
