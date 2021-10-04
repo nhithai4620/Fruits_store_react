2bf0269399a5256d871b65ecc2a21b6371f7bbf9
Nguyễn Nhĩ Thái <62824044+nhithai4620@users.noreply.github.com>
2021-10-04 20:31:59 +0700

    thai commit at 7:51 10/4/2021

--- src/components/section/About.js
+++ src/components/section/About.js
@@ -9,12 +9,21 @@ export class About extends React.Component{
              <div className="padding-header" style={{background:`${this.context.theme}`}}>
 
             </div>
-            <div className="about" id="about">
-                <h3>Welcome customer!</h3>
-                <p> This website is created by Nguyễn Nhĩ Thái</p>
-                <p> I am a student of Cần Thơ university</p>
-                <p> This is a ecommerce website project which I created when I was learning the basic academic subject</p>
-            </div>    
+            <div className="about" style={{backgroundImage: `url("./assets/about_bg.jpg")`}}>
+                <div className="about-img">
+                <img src="./assets/thedeliver.png" alt />
+                </div>
+                <div className="about-content">
+                <h3>
+                    ABOUT US
+                </h3>
+                <p>
+                    One advanced diverted domestic sex repeat bringing you old. Possible procured her trifing laughter thought property she met way.
+                    Companions shy had solicitude favourable own. Which could saw guest man how heart but. Lasted my comming uneasy marked so should.
+                    Gravity letters it amongst herself dearest an windows by. Wooded ladies she basket season age her uneasy saw. Discource unwilling am no described incommode no listening of. Before nature his parish boy.
+                </p>
+                </div>
+            </div>
             </>    
         );
     }
--- src/components/section/Contact.js
+++ src/components/section/Contact.js
@@ -4,14 +4,24 @@ import {DataContext} from '../Context';
 export class Contact extends React.Component{
     static contextType = DataContext;
     render(){
-        const {colors} = this.props;
-        
+        var color = this.context.theme;
+        var Bg = "../assets/apple_bg.png";
+        if (color == "orange"){
+          Bg = "../assets/orange_bg.png";
+        } else if (color == "yellow"){
+          Bg = "../assets/banana_bg.png";
+        } else if (color == "#42cc14"){
+          Bg = "../assets/apple_bg.png";
+        } else if (color == "#ff4000"){
+          Bg = "../assets/straw_bg.png";
+        }
         return (
             <>
             <div className="padding-header" style={{background:`${this.context.theme}`}}>
 
-            </div>    
-            <div className="contact">
+            </div>
+             
+            <div className="contact"  style={{backgroundImage: `url(${Bg})`}}>
                 <div className="contact-text" id="contact">
                 <h1>Contact</h1>
                 Want to work together?  Get in touch!
