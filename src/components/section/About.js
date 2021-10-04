3202d530448892f92cc2179a4def53ce4afdd53e
Nguyễn Nhĩ Thái <62824044+nhithai4620@users.noreply.github.com>
2021-10-04 19:51:31 +0700

    thai commit at 7:51 10/4/2021

--- src/components/section/About.js
+++ src/components/section/About.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import '../css/About.css';
+import {DataContext} from '../Context';
 export class About extends React.Component{
+    static contextType = DataContext;
     render(){
         return (
+            <>
+             <div className="padding-header" style={{background:`${this.context.theme}`}}>
+
+            </div>
             <div className="about" id="about">
                 <h3>Welcome customer!</h3>
                 <p> This website is created by Nguyễn Nhĩ Thái</p>
                 <p> I am a student of Cần Thơ university</p>
                 <p> This is a ecommerce website project which I created when I was learning the basic academic subject</p>
-            </div>        
+            </div>    
+            </>    
         );
     }
 }
